753569619c04fe64a44dbdd5984d4a218f4991da
Nathan <52774360+NDB-7@users.noreply.github.com>
2026-06-07 12:35:50 -0700

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+
+        for (int[] p : prerequisites) adj.get(p[1]).add(p[0]);
+
+        int[] visitState = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) if (hasCycle(i, adj, visitState)) return false;
+
+        return true;
+    }
+
+    // Visit state: 0 = unvisited, 1 = visiting, 2 = visited
+    private boolean hasCycle(int i, List<List<Integer>> adj, int[] visitState) {
+        if (visitState[i] == 2) return false;
+        if (visitState[i] == 1) return true;
+
+        visitState[i] = 1;
+        for (int n : adj.get(i)) if (hasCycle(n, adj, visitState)) return true;
+
+        visitState[i] = 2;
+        return false;
+    }
+}
