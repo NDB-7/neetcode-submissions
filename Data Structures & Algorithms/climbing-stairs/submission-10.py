3d6291a4c7aeda4cf93c85088faf6f9aa1f4643f
Nathan <52774360+NDB-7@users.noreply.github.com>
2026-07-22 13:18:41 -0700

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        table = {}
+        table[1] = 1
+        table[2] = 2
+
+        for i in range(3, n + 1):
+            table[i] = table[i - 1] + table[i - 2]
+        
+        return table[n]
