1b99ca007a7e52a0b3b62f30a7f420ee54ca797c
Nathan <52774360+NDB-7@users.noreply.github.com>
2026-08-27 14:55:22 -0700

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def partition(start, end):
+            pivot = nums[(start + end) // 2]
+            l, r = start - 1, end + 1
+
+            while l < r:
+                while True:
+                    l += 1
+                    if nums[l] >= pivot:
+                        break
+
+                while True:
+                    r -= 1
+                    if nums[r] <= pivot:
+                        break
+
+                if l >= r:
+                    return r
+                
+                nums[l], nums[r] = nums[r], nums[l]
+
+            return r
+
+        def quickSort(start, end):
+            if start >= end:
+                return
+            i = partition(start, end)
+            quickSort(start, i)
+            quickSort(i + 1, end)
+
+        quickSort(0, len(nums) - 1)
+        return nums
